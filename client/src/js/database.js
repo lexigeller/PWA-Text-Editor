4161ec5069ebddaaa5636e00883b51ca4329e590
Lexi Geller <leximoconner@yahoo.com>
2023-08-27 21:52:57 -0400

    rewrote database using code from 19.23 for example

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,34 +12,26 @@ const initdb = async () =>
     },
   });
 
-// Method that accepts content and adds it to the database
 export const putDb = async (content) => {
-  try {
-    const jateDB = await openDB('jate', 1);
-    const tx = jateDB.transaction('jate', 'readwrite');
-    const store = tx.objectStore('jate');
-    const request = store.put({ value: content });
-
-    await tx.done;
-    console.log('Data added to the database');
-  } catch (error) {
-    console.error(error);
-  }
+  console.log('Put to the db');
+  
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('data saved to the database', result);
 };
 
-// Method that gets all the content from the database
 export const getDb = async () => {
-  try {
-    const jateDB = await openDB('jate', 1);
-    const tx = jateDB.transaction('jate', 'readonly');
-    const store = tx.objectStore('jate');
-    const request = store.get(1);
-
-    const result = await request;
-    return result?.value;
-  } catch (error) {
-    console.error(error);
-  }
+  console.log('GET all from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
 
 initdb();
